7f0545267f74bdf610112160ecbdbd004b8b316c
Jojanneke Tamis <jojanneketamis@hotmail.com>
2016-05-03 09:22:13 +0200

    projects work

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,36 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "240px";
+    document.getElementById("main").style.marginLeft = "240px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+$(document).ready(function() {
+
+ $('a[href*="#"]:not([href="#"])').click(function() {
+   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+     var target = $(this.hash);
+     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+     if (target.length) {
+       $('html, body').animate({
+         scrollTop: target.offset().top
+       }, 1000);
+       return false;
+     }
+   }
+ });
+
+});
+
+$(document).ready(function(){
+  $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+    $(this).toggleClass('open');
+  });
+});
